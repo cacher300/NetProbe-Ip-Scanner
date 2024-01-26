24f46daf1951ce8f8a9c1e618d76bfd1e2461f83
Personal website <cacher300@gmail.com>
2024-01-25 23:13:48 -0400

    we can now scan ports

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import socket
 import ipaddress
+import csv
 
 
 def get_local_ip():
@@ -11,10 +12,57 @@ def get_local_ip():
 
     return IP
 
+def get_ip_range():
+    local_ip = get_local_ip()
+    ip_with_mask = str(local_ip) + "/24"  # I am assuming we are using /24 here for simplicity but later i can add support for more although idk how to test that
 
-local_ip = get_local_ip()
-ip_with_mask = str(local_ip) + "/24"  # I am assuming we are using /24 here for simplicity but later i can add support for more although idk how to test that
+    network = ipaddress.ip_network(ip_with_mask, strict=False)
 
-network = ipaddress.ip_network(ip_with_mask, strict=False)
+    ip_range = [str(network.network_address + 1), str(network.broadcast_address - 1)]
 
-ip_range = [network.network_address + 1, network.broadcast_address - 1]
+    print(f"Network Address: {network.network_address}")
+    print(f"First Usable IP: {network.network_address + 1}")
+    print(f"Last Usable IP: {network.broadcast_address - 1}")
+    print(f"Broadcast Address: {network.broadcast_address}")
+
+    return ip_range
+
+
+def scan_port(ip, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    s.settimeout(1)
+
+    try:
+        s.connect((ip, port))
+        s.close()
+        return True
+    except (socket.timeout, socket.error):
+        return False
+
+
+ip_list = get_ip_range()
+
+base_ip = ip_list[0]
+
+parts = base_ip.split('.')
+base_ip = '.'.join(parts[:-1]) + '.'
+print(base_ip)
+
+with open("open_ports.csv", mode="w", newline='') as file:
+    writer = csv.writer(file)
+    #Header
+    writer.writerow(["IP Address", "Port", "Status"])
+
+    for i in range (1, 255):
+        ip_address = base_ip + str(i)
+        port_to_scan = 80
+
+        if scan_port(ip_address, port_to_scan):
+            print(f"Port {port_to_scan} is open on {ip_address}")
+            writer.writerow([ip_address, port_to_scan, "Open"])
+        else:
+            # Uncomment the next line if you want to record closed ports as well
+            # writer.writerow([ip_address, port_to_scan, "Closed/Filtered"])
+            continue
+
